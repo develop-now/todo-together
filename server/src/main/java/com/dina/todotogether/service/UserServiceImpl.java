f1b29cfd0530ffdfe49035f6b19896eb0f383eff
jieun <developnow55@gmail.com>
2022-02-09 01:25:44 +0900

    Add passwordEncoder

--- server/src/main/java/com/dina/todotogether/service/UserServiceImpl.java
+++ server/src/main/java/com/dina/todotogether/service/UserServiceImpl.java
@@ -8,6 +8,9 @@ import com.dina.todotogether.repo.MemberInfoRepo;
 import com.dina.todotogether.repo.UserRepo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -18,16 +21,18 @@ import javax.transaction.Transactional;
 public class UserServiceImpl implements UserService{
     private final UserRepo userRepo;
     private final MemberInfoRepo memberInfoRepo;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepo userRepo, MemberInfoRepo memberInfoRepo) {
+    public UserServiceImpl(UserRepo userRepo, MemberInfoRepo memberInfoRepo, PasswordEncoder passwordEncoder) {
         this.userRepo = userRepo;
         this.memberInfoRepo = memberInfoRepo;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void signUp(MemberSignUpRequest member, MemberInfoSignUpRequest memberInfo) {
-
+        member.setPassword(passwordEncoder.encode(member.getPassword()));
         AllUser saveMember = member.toEntity();
         MemberInfo saveMemberInfo = memberInfo.toEntity(saveMember);
         AllUser memberResult = userRepo.save(saveMember);
