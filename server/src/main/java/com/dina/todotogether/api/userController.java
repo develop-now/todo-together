16313af09881324885c8e391cee99e8f620f3582
jieun <developnow55@gmail.com>
2022-02-13 16:37:18 +0900

    Add AmazonS3 basic uploading user profile

--- server/src/main/java/com/dina/todotogether/api/userController.java
+++ server/src/main/java/com/dina/todotogether/api/userController.java
@@ -2,6 +2,7 @@ package com.dina.todotogether.api;
 
 import com.dina.todotogether.data.dto.MemberInfoSignUpRequest;
 import com.dina.todotogether.data.dto.MemberSignUpRequest;
+import com.dina.todotogether.service.S3Service;
 import com.dina.todotogether.service.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +19,18 @@ public class userController {
     @Autowired
     private UserServiceImpl userServiceImpl;
 
+    @Autowired
+    private S3Service s3Service;
+
     @PostMapping("/signUp")
-    public void signUp (@Valid MemberSignUpRequest member, @Valid MemberInfoSignUpRequest memberInfo) {
+    public void signUp (@Valid MemberSignUpRequest member, @Valid MemberInfoSignUpRequest memberInfo) throws Exception {
+        MultipartFile uploadProfile = memberInfo.getUploadProfile();
+
+        if(uploadProfile != null) {
+            s3Service.UploadFile(uploadProfile);
+            memberInfo.setOriginalProfile(uploadProfile.getOriginalFilename());
+        }
         userServiceImpl.signUp(member, memberInfo);
     }
+
 }
